a5dba0647f3f082ba1cf41e185c30b278e43998d
Alex_adevart <saniokff@gmail.com>
2018-05-28 21:14:09 +0300

    - 3d circular loader simple example

---
+++ LoaderUnity3d/Assets/3DLoader/Scripts/UIHandler.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class UIHandler : MonoBehaviour {
+
+    
+    public GameObject circle3dLoader;
+    public GameObject ciclle2dLoader;
+   
+    public Text switchBtnText;
+    public Text loadingText;
+
+    private bool is3dState = true;
+    private float progressCounter;
+
+    private void Start()
+    {
+        StartCoroutine(StartProgressCoroutine());
+    }
+
+    public void OnSwitchClick() {
+        is3dState = !is3dState;
+        circle3dLoader.SetActive(is3dState);
+        ciclle2dLoader.SetActive(!is3dState);
+        if (is3dState) {
+            switchBtnText.text = "3D";
+        }
+        else {
+            switchBtnText.text = "2D";
+        }
+    }
+
+    public void StartProgress() {
+        StartCoroutine(StartProgressCoroutine());
+    }
+
+    public IEnumerator StartProgressCoroutine() {
+        while (progressCounter < 100) {
+            progressCounter = progressCounter + 1f ;
+            loadingText.text = "LOADING " + progressCounter + "%";
+            yield return new WaitForSeconds(0.1f);
+        }
+        SceneManager.LoadScene("Welcome", LoadSceneMode.Single);
+    }
+
+    
+}
